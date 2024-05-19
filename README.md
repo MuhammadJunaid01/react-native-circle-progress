2e70e901221fec546d4abb3ccac84b1614d28702
MuhammadJunaid01 <m.junaidbkh2020@gmail.com>
2024-05-19 20:38:07 +0600

    change package name

--- README.md
+++ README.md
@@ -1,7 +1,6 @@
 [![license](https://img.shields.io/github/license/mashape/apistatus.svg)]()
-[![Version](https://img.shields.io/npm/v/react-native-progress-circle.svg)](https://www.npmjs.com/package/react-native-progress-circle)
-[![npm](https://img.shields.io/npm/dt/react-native-progress-circle.svg)](https://www.npmjs.com/package/react-native-progress-circle)
-[![Twitter Follow](https://img.shields.io/twitter/follow/cmichelio.svg?style=social&label=Follow)](https://twitter.com/cmichelio)
+[![npm](https://img.shields.io/npm/dt/rn-circle-progress.svg)](https://www.npmjs.com/package/rn-circle-progress)
+[![LinkedIn Follow](https://img.shields.io/badge/LinkedIn-Connect-blue?style=social&logo=linkedin)](https://www.linkedin.com/in/mj-juanaid-7358b5216/)
 
 # React Native Progress Circle
 
@@ -9,59 +8,54 @@
 
 ## Features
 
-* Custom colors
-* Custom size and border radius
-* Light-weight: No other dependencies besides `react-native`
+- Custom colors
+- Custom size and border radius
+- Light-weight: No other dependencies besides `react-native`
 
 ## Installation
 
-`yarn add react-native-progress-circle`
+`yarn add rn-circle-progress`
 
 or
 
-`npm install --save react-native-progress-circle`
+`npm install rn-circle-progress`
 
 ## Usage
 
 ```javascript
-import ProgressCircle from 'react-native-progress-circle'
+import ProgressCircle from "rn-circle-progress";
 
-render() {
-    return (
-        <ProgressCircle
-            percent={30}
-            radius={50}
-            borderWidth={8}
-            color="#3399FF"
-            shadowColor="#999"
-            bgColor="#fff"
-        >
-            <Text style={{ fontSize: 18 }}>{'30%'}</Text>
-        </ProgressCircle>
-    )
-}
+return (
+  <ProgressCircle
+    percent={30}
+    radius={50}
+    borderWidth={8}
+    color="#3399FF"
+    shadowColor="#999"
+    bgColor="#fff"
+  >
+    <Text style={{ fontSize: 18 }}>{"30%"}</Text>
+  </ProgressCircle>
+);
 ```
 
 ## Props
-| Name | Description | Type | Required | Default Value |
-| :--- | :----- | :--- | :---: | :---: |
-| percent | The percentage used for displaying the progress | Number | ✓ |  |
-| radius | The radius in `px` of the component (including border) | Number | ✓ |  |
-| borderWidth | The border width in `px` | Number | ✓ |  |
-| color | The border color | String |  | ![#f00](https://placehold.it/15/f00/000000?text=+) `'#f00'` |
-| shadowColor | The background color of the border | String |  | ![#999](https://placehold.it/15/999/000000?text=+) `'#999'` |
-| bgColor | The inner background color of the component  | String |  | ![#e9e9ef](https://placehold.it/15/e9e9ef/000000?text=+) `'#e9e9ef'` |
-| children | The children to render inside this component | Node |  | `null` |
-| containerStyle | The custom styling which will be applied to the container of the `children` | Style |  | `null` |
-| outerCircleStyle | The custom styling which will be applied to the outer circle | Style |  | `null` |
+
+| Name             | Description                                                                 | Type   | Required |                            Default Value                             |
+| :--------------- | :-------------------------------------------------------------------------- | :----- | :------: | :------------------------------------------------------------------: |
+| percent          | The percentage used for displaying the progress                             | Number |    ✓     |                                                                      |
+| radius           | The radius in `px` of the component (including border)                      | Number |    ✓     |                                                                      |
+| borderWidth      | The border width in `px`                                                    | Number |    ✓     |                                                                      |
+| color            | The border color                                                            | String |          |     ![#f00](https://placehold.it/15/f00/000000?text=+) `'#f00'`      |
+| shadowColor      | The background color of the border                                          | String |          |     ![#999](https://placehold.it/15/999/000000?text=+) `'#999'`      |
+| bgColor          | The inner background color of the component                                 | String |          | ![#e9e9ef](https://placehold.it/15/e9e9ef/000000?text=+) `'#e9e9ef'` |
+| children         | The children to render inside this component                                | Node   |          |                                `null`                                |
+| containerStyle   | The custom styling which will be applied to the container of the `children` | Style  |          |                                `null`                                |
+| outerCircleStyle | The custom styling which will be applied to the outer circle                | Style  |          |                                `null`                                |
 
 ## Author
 
-[Christoph Michel](http://cmichel.io)
-
-## Implementation Details
-
-[React Native Progress Circle](http://cmichel.io/react-native-progress-circle)
+[M JUNAID](https://dev-junaid-portfolio-v2.vercel.app/)
 
 ## License
 
--- dist/index.js
+++ dist/index.js
@@ -1,24 +1,18 @@
 "use strict";
-var __assign =
-  (this && this.__assign) ||
-  function () {
-    __assign =
-      Object.assign ||
-      function (t) {
+var __assign = (this && this.__assign) || function () {
+    __assign = Object.assign || function(t) {
         for (var s, i = 1, n = arguments.length; i < n; i++) {
-          s = arguments[i];
-          for (var p in s)
-            if (Object.prototype.hasOwnProperty.call(s, p)) t[p] = s[p];
+            s = arguments[i];
+            for (var p in s) if (Object.prototype.hasOwnProperty.call(s, p))
+                t[p] = s[p];
         }
         return t;
-      };
+    };
     return __assign.apply(this, arguments);
-  };
-var __importDefault =
-  (this && this.__importDefault) ||
-  function (mod) {
-    return mod && mod.__esModule ? mod : { default: mod };
-  };
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 var _a, _b;
 Object.defineProperty(exports, "__esModule", { value: true });
 var react_1 = __importDefault(require("react"));
@@ -26,157 +20,100 @@ var react_native_1 = require("react-native");
 var direction = react_native_1.I18nManager.isRTL ? "right" : "left";
 var opDirection = react_native_1.I18nManager.isRTL ? "Left" : "Right";
 var styles = react_native_1.StyleSheet.create({
-  outerCircle: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  innerCircle: {
-    overflow: "hidden",
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  leftWrap:
-    ((_a = {
-      position: "absolute",
-      top: 0,
-    }),
-    (_a["".concat(direction)] = 0),
-    _a),
-  halfCircle:
-    ((_b = {
-      position: "absolute",
-      top: 0,
-      left: 0,
-    }),
-    (_b["borderTop".concat(opDirection, "Radius")] = 0),
-    (_b["borderBottom".concat(opDirection, "Radius")] = 0),
-    _b),
-});
-var percentToDegrees = function (percent) {
-  return percent * 3.6;
-};
-var PercentageCircle = function (_a) {
-  var _b = _a.color,
-    color = _b === void 0 ? "#f00" : _b,
-    _c = _a.shadowColor,
-    shadowColor = _c === void 0 ? "#999" : _c,
-    _d = _a.bgColor,
-    bgColor = _d === void 0 ? "#e9e9ef" : _d,
-    radius = _a.radius,
-    _e = _a.borderWidth,
-    borderWidth = _e === void 0 ? 2 : _e,
-    percent = _a.percent,
-    _f = _a.children,
-    children = _f === void 0 ? null : _f,
-    _g = _a.containerStyle,
-    containerStyle = _g === void 0 ? null : _g,
-    _h = _a.outerCircleStyle,
-    outerCircleStyle = _h === void 0 ? null : _h;
-  var _j = react_1.default.useState(0),
-    halfCircle1Degree = _j[0],
-    setHalfCircle1Degree = _j[1];
-  var _k = react_1.default.useState(0),
-    halfCircle2Degree = _k[0],
-    setHalfCircle2Degree = _k[1];
-  var _l = react_1.default.useState({
-      backgroundColor: color,
-    }),
-    halfCircle2Styles = _l[0],
-    setHalfCircle2Styles = _l[1];
-  react_1.default.useEffect(
-    function () {
-      var clampedPercent = Math.max(Math.min(100, percent), 0);
-      var needHalfCircle2 = clampedPercent > 50;
-      var halfCircle1Degree;
-      var halfCircle2Degree;
-      if (needHalfCircle2) {
-        halfCircle1Degree = 180;
-        halfCircle2Degree = percentToDegrees(clampedPercent);
-      } else {
-        halfCircle1Degree = percentToDegrees(clampedPercent);
-        halfCircle2Degree = 0;
-      }
-      setHalfCircle1Degree(halfCircle1Degree);
-      setHalfCircle2Degree(halfCircle2Degree);
-      setHalfCircle2Styles({
-        backgroundColor: needHalfCircle2 ? color : shadowColor,
-      });
+    outerCircle: {
+        justifyContent: "center",
+        alignItems: "center",
     },
-    [percent, color, shadowColor]
-  );
-  var renderHalfCircle = function (rotateDegrees, halfCircleStyles) {
-    if (halfCircleStyles === void 0) {
-      halfCircleStyles = {};
-    }
-    return react_1.default.createElement(
-      react_native_1.View,
-      {
-        style: [
-          styles.leftWrap,
-          {
-            width: radius,
-            height: radius * 2,
-          },
-        ],
-      },
-      react_1.default.createElement(react_native_1.View, {
-        style: [
-          styles.halfCircle,
-          __assign(
-            {
-              width: radius,
-              height: radius * 2,
-              borderRadius: radius,
-              overflow: "hidden",
-              transform: [
-                { translateX: radius / 2 },
-                { rotate: "".concat(rotateDegrees, "deg") },
-                { translateX: -radius / 2 },
-              ],
-              backgroundColor: color,
-            },
-            halfCircleStyles
-          ),
-        ],
-      })
-    );
-  };
-  var renderInnerCircle = function () {
-    var radiusMinusBorder = radius - borderWidth;
-    return react_1.default.createElement(
-      react_native_1.View,
-      {
-        style: [
-          styles.innerCircle,
-          {
-            width: radiusMinusBorder * 2,
-            height: radiusMinusBorder * 2,
-            borderRadius: radiusMinusBorder,
-            backgroundColor: bgColor,
-          },
-          containerStyle,
-        ],
-      },
-      children
-    );
-  };
-  return react_1.default.createElement(
-    react_native_1.View,
-    {
-      style: [
-        styles.outerCircle,
-        {
-          width: radius * 2,
-          height: radius * 2,
-          borderRadius: radius,
-          backgroundColor: shadowColor,
+    innerCircle: {
+        overflow: "hidden",
+        justifyContent: "center",
+        alignItems: "center",
+    },
+    leftWrap: (_a = {
+            position: "absolute",
+            top: 0
         },
-        outerCircleStyle,
-      ],
-    },
-    renderHalfCircle(halfCircle1Degree),
-    renderHalfCircle(halfCircle2Degree, halfCircle2Styles),
-    renderInnerCircle()
-  );
+        _a["".concat(direction)] = 0,
+        _a),
+    halfCircle: (_b = {
+            position: "absolute",
+            top: 0,
+            left: 0
+        },
+        _b["borderTop".concat(opDirection, "Radius")] = 0,
+        _b["borderBottom".concat(opDirection, "Radius")] = 0,
+        _b),
+});
+var percentToDegrees = function (percent) { return percent * 3.6; };
+var PercentageCircle = function (_a) {
+    var _b = _a.color, color = _b === void 0 ? "#f00" : _b, _c = _a.shadowColor, shadowColor = _c === void 0 ? "#999" : _c, _d = _a.bgColor, bgColor = _d === void 0 ? "#e9e9ef" : _d, radius = _a.radius, _e = _a.borderWidth, borderWidth = _e === void 0 ? 2 : _e, percent = _a.percent, _f = _a.children, children = _f === void 0 ? null : _f, _g = _a.containerStyle, containerStyle = _g === void 0 ? null : _g, _h = _a.outerCircleStyle, outerCircleStyle = _h === void 0 ? null : _h;
+    var _j = react_1.default.useState(0), halfCircle1Degree = _j[0], setHalfCircle1Degree = _j[1];
+    var _k = react_1.default.useState(0), halfCircle2Degree = _k[0], setHalfCircle2Degree = _k[1];
+    var _l = react_1.default.useState({
+        backgroundColor: color,
+    }), halfCircle2Styles = _l[0], setHalfCircle2Styles = _l[1];
+    react_1.default.useEffect(function () {
+        var clampedPercent = Math.max(Math.min(100, percent), 0);
+        var needHalfCircle2 = clampedPercent > 50;
+        var halfCircle1Degree;
+        var halfCircle2Degree;
+        if (needHalfCircle2) {
+            halfCircle1Degree = 180;
+            halfCircle2Degree = percentToDegrees(clampedPercent);
+        }
+        else {
+            halfCircle1Degree = percentToDegrees(clampedPercent);
+            halfCircle2Degree = 0;
+        }
+        setHalfCircle1Degree(halfCircle1Degree);
+        setHalfCircle2Degree(halfCircle2Degree);
+        setHalfCircle2Styles({
+            backgroundColor: needHalfCircle2 ? color : shadowColor,
+        });
+    }, [percent, color, shadowColor]);
+    var renderHalfCircle = function (rotateDegrees, halfCircleStyles) {
+        if (halfCircleStyles === void 0) { halfCircleStyles = {}; }
+        return (react_1.default.createElement(react_native_1.View, { style: [
+                styles.leftWrap,
+                {
+                    width: radius,
+                    height: radius * 2,
+                },
+            ] },
+            react_1.default.createElement(react_native_1.View, { style: [
+                    styles.halfCircle,
+                    __assign({ width: radius, height: radius * 2, borderRadius: radius, overflow: "hidden", transform: [
+                            { translateX: radius / 2 },
+                            { rotate: "".concat(rotateDegrees, "deg") },
+                            { translateX: -radius / 2 },
+                        ], backgroundColor: color }, halfCircleStyles),
+                ] })));
+    };
+    var renderInnerCircle = function () {
+        var radiusMinusBorder = radius - borderWidth;
+        return (react_1.default.createElement(react_native_1.View, { style: [
+                styles.innerCircle,
+                {
+                    width: radiusMinusBorder * 2,
+                    height: radiusMinusBorder * 2,
+                    borderRadius: radiusMinusBorder,
+                    backgroundColor: bgColor,
+                },
+                containerStyle,
+            ] }, children));
+    };
+    return (react_1.default.createElement(react_native_1.View, { style: [
+            styles.outerCircle,
+            {
+                width: radius * 2,
+                height: radius * 2,
+                borderRadius: radius,
+                backgroundColor: shadowColor,
+            },
+            outerCircleStyle,
+        ] },
+        renderHalfCircle(halfCircle1Degree),
+        renderHalfCircle(halfCircle2Degree, halfCircle2Styles),
+        renderInnerCircle()));
 };
 exports.default = PercentageCircle;
